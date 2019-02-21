8271026b6328543640cc5080aaedd6ed0b110b4d
Mateusz Kaczmarek <mateuszk131@gmail.com>
2019-02-21 15:36:09 +0100

    1.0.2

--- Youtube downloader/Form1.Designer.cs
+++ Youtube downloader/Form1.Designer.cs
@@ -34,12 +34,15 @@
             this.buttonTrimmer = new System.Windows.Forms.Button();
             this.buttonStart = new System.Windows.Forms.Button();
             this.buttonMP3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(324, 20);
             this.textBox1.TabIndex = 1;
@@ -47,10 +50,10 @@
             // axWindowsMediaPlayer1
             // 
             this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-2, 108);
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-2, 137);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(800, 341);
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(859, 369);
             this.axWindowsMediaPlayer1.TabIndex = 2;
             // 
             // buttonTrimmer
@@ -65,7 +68,8 @@
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(101, 55);
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(136, 86);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(75, 23);
             this.buttonStart.TabIndex = 4;
@@ -76,7 +80,7 @@
             // buttonMP3
             // 
             this.buttonMP3.Enabled = false;
-            this.buttonMP3.Location = new System.Drawing.Point(246, 54);
+            this.buttonMP3.Location = new System.Drawing.Point(255, 86);
             this.buttonMP3.Name = "buttonMP3";
             this.buttonMP3.Size = new System.Drawing.Size(90, 23);
             this.buttonMP3.TabIndex = 5;
@@ -84,11 +88,42 @@
             this.buttonMP3.UseVisualStyleBackColor = true;
             this.buttonMP3.Click += new System.EventHandler(this.buttonMP3_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Wybór ścieżki";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "C:\\";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ścieżka:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.ClientSize = new System.Drawing.Size(859, 509);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonMP3);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.buttonTrimmer);
@@ -109,6 +144,9 @@
         private System.Windows.Forms.Button buttonTrimmer;
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Button buttonMP3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Youtube downloader/Form1.cs
+++ Youtube downloader/Form1.cs
@@ -10,24 +10,28 @@ namespace Youtube_downloader
     public partial class Form1 : Form
     {
         public Form1()
-        {           
+        {
             InitializeComponent();
             buttonStart.Text = GUI.OpisElementowGUI.ButtonStart;
             buttonTrimmer.Text = GUI.OpisElementowGUI.ButtonTrimmer;
-            buttonMP3.Text=GUI.OpisElementowGUI.ButtonMP3;
-        }        
+            buttonMP3.Text = GUI.OpisElementowGUI.ButtonMP3;
+
+        }
+        
         string link = "";
-        string sciezka = "";        
+        string sciezka = "";
         private void buttonStart_Click(object sender, EventArgs e)
         {
             if (textBox1.Text.Contains("youtube"))
             {
+
+
                 link = textBox1.Text;
-                var source = @"D:\\";
+                var source = label1.Text;
                 var youtube = YouTube.Default;
                 var vid = youtube.GetVideo(link);
                 sciezka = source + vid.FullName.Substring(0, vid.FullName.Length - 14) + ".mp4";
-                File.WriteAllBytes(sciezka, vid.GetBytes());               
+                File.WriteAllBytes(sciezka, vid.GetBytes());
                 axWindowsMediaPlayer1.URL = sciezka;
                 buttonMP3.Enabled = true;
             }
@@ -43,13 +47,13 @@ namespace Youtube_downloader
         }
         private void buttonMP3_Click(object sender, EventArgs e)
         {
-            axWindowsMediaPlayer1.Ctlcontrols.pause();            
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
             var youtube = YouTube.Default;
             var vid = youtube.GetVideo(link);
-           //File.Copy(source+vid.FullName,source+"copy "+vid.FullName);
-            var inputFile = new MediaFile { Filename = sciezka};
+            //File.Copy(source+vid.FullName,source+"copy "+vid.FullName);
+            var inputFile = new MediaFile { Filename = sciezka };
             //sciezka = sciezka.Substring(0, sciezka.Length - 4);
-            var outputFile = new MediaFile { Filename = $"{sciezka.Substring(0,sciezka.Length-4)}.mp3" };
+            var outputFile = new MediaFile { Filename = $"{sciezka.Substring(0, sciezka.Length - 4)}.mp3" };
             using (var engine = new Engine())
             {
                 engine.GetMetadata(inputFile);
@@ -57,5 +61,20 @@ namespace Youtube_downloader
             }
             axWindowsMediaPlayer1.Ctlcontrols.play();
         }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            using (var fldrDlg = new FolderBrowserDialog())
+            {
+
+
+                if (fldrDlg.ShowDialog() == DialogResult.OK)
+                {
+                    buttonStart.Enabled = true;
+                    label1.Text = fldrDlg.SelectedPath;
+                }
+            }
+        }
     }
 }
+    
